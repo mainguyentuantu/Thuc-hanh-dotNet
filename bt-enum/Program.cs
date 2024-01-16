39578edba4d1ad1f6badfe289c78284c11f23446
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-16 09:02:56 +0700

    bt-enum

---
+++ bt-enum/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program{
+
+enum HOCLUC{Kem, TrungBinh, Kha, Gioi}
+
+static void Main(string[] args)
+{
+    HOCLUC hocluc;
+    hocluc = HOCLUC.Kha;
+
+    switch(hocluc){
+            case HOCLUC.Kem:
+             Console.WriteLine("Hoc luc kem");
+             break;
+
+            case HOCLUC.TrungBinh: Console.WriteLine("Hoc luc trung binh");
+            break;
+            case HOCLUC.Kha: Console.WriteLine("Hoc luc kha");
+            break;
+            case HOCLUC.Gioi: Console.WriteLine("Hoc luc gioi");
+            break;
+    }
+
+
+}
+}
