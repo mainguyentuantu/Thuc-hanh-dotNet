446670464ae3605777f5b6234f6feaab52e95144
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-19 09:33:46 +0700

    bt-generic

---
+++ bt-generic/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Programs{
+class Programs{
+class Product<A>{
+    A ID;
+
+    public void SetID(A _id){
+        this.ID = _id ;
+    }
+
+    public void PrintInf(){
+        System.Console.WriteLine($"ID = {this.ID}");
+    }
+
+}
+
+static void Main(string[] args)
+{
+    Product<int> sanpham1 = new Product<int>();
+
+    sanpham1.SetID(123);
+    sanpham1.PrintInf();
+
+    Product<String> sanpham2 = new Product<string>();
+
+    sanpham2.SetID("tuan tu");
+    sanpham2.PrintInf();
+
+}
+}
+}
