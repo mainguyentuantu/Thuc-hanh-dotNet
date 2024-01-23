dcbcc60d9efd30c2f256024fe4e0f74439c129a4
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-23 17:12:02 +0700

    vi du Delegate

---
+++ bt-delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Delegate
+{
+    public delegate void ShowLog(string message);
+
+    public class Program
+    {
+        static void Info(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        static void Warning(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+
+        static void Main(string[] args)
+        {
+            ShowLog log = null;
+
+            log = Info;
+            log?.Invoke("Xin Chao ABC");
+
+            log = Warning;
+            log("Hoc ve Delegate");
+
+
+
+            log += Info;
+            log += Warning;
+            log.Invoke("Xin chao tuantu");
+
+        }
+    }
+}
