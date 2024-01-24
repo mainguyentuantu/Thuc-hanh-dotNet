b5aa8fa0490acc0313bd894e2deff538c58ab203
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-24 17:25:16 +0700

    vi du ve extension method

---
+++ bt-extension-method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyLib;
+
+namespace ExtendsionMethod
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            double a = 60.5;
+            Console.WriteLine(a.BinhPhuong());
+            Console.WriteLine(a.CanBacHai());
+            Console.WriteLine(a.Sin());
+            Console.WriteLine(a.Cos());
+
+
+        }
+    }
+}
