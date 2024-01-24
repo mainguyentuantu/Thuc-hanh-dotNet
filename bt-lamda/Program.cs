c89d4698632726547509f9752b2e0a6f3df767c8
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-24 09:46:40 +0700

    vi du ve lamda va cach su dung

---
+++ bt-lamda/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Action<string> welcome;
+        welcome = s => Console.WriteLine(s);
+        welcome?.Invoke("Xin chao ban");
+
+        Action<string,string> ten;
+        ten = (mgs, name) =>
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(mgs + " " + name);
+            Console.ResetColor();
+        };
+        ten?.Invoke("Chao ban", "tuantu");
+
+        Func<int, int, int> tinhtoan;
+        tinhtoan = (a, b) =>
+        {
+            int kq = a + b;
+            return kq;
+        };
+       Console.WriteLine(tinhtoan?.Invoke(9, 5));
+
+        int[] mang = { 1, 2, 3, 4, 5, 6, 7 };
+
+        var kq = mang.Select( (int x) =>
+        {
+            return Math.Sqrt(x);
+        });
+
+        foreach (var x in kq) { Console.WriteLine(x); }
+    }
+}
