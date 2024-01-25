9bf913c38ce148da8de4b68fec230ac28ecf84e5
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-25 14:04:07 +0700

    vi du ve Exception

---
+++ bt-Exception/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace btngoaile{
+
+    class Programe
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 0;
+
+            //Excaption
+
+
+            try
+            {
+                var c = a / b;                  //phat sih doi tuong Exception, ke thua Exception
+                Console.WriteLine(c);
+
+            }
+            catch(Exception e) 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.GetType().Name);
+                Console.WriteLine("Phep chia co loi.");
+                Console.ResetColor();
+            }
+        finally { Console.WriteLine("Ket Thuc!"); }
+        
+        }
+    }
+}
