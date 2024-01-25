db526d54cbf23951060773cc036883789bf7e0c4
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-25 15:22:02 +0700

    vi du Exception

---
+++ bt-Exception-3/MyExceptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt_Exception_3
+{
+    public class NameEmptyException : Exception
+    {
+        public NameEmptyException() : base("Ten ko duoc rong...")
+        { 
+
+        }
+    }
+
+    public class AgeException : Exception
+    {
+        public int age { get; set; }
+
+        public AgeException(int _age) : base("Tuoi ko phu hop...")
+        {
+            age = _age;
+        }
+
+        public void Detail()
+        {
+            Console.WriteLine($"Tuoi = {age}, khong nam trong khoang [18 - 100]");
+        }
+
+      
+     }
+
+}
+
---
+++ bt-Exception-3/Program.cs
@@ -0,0 +1,39 @@
+﻿using bt_Exception_3;
+using System;
+
+namespace btEx
+{
+    class Program
+    {
+        static void Register(string name, int age)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NameEmptyException();
+            }
+            if (age<18 || age > 100)
+            {
+                throw new AgeException(age);
+            }
+            //...
+            Console.WriteLine($"Xin chao {name} ({age} tuoi)");
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Register("tuantu", 123);
+            }
+            catch (NameEmptyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(AgeException e)
+            {
+                Console.WriteLine(e.Message);
+                e.Detail();
+            }
+        }
+    }
+    
+ }
