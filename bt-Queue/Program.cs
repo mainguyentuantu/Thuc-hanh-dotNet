bae0ce453bad5f276e4fffb3f627e52cb2aeac57
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-26 14:45:49 +0700

    vi du ve cau truc Queue

---
+++ bt-Queue/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace btQueue
+{
+    class Program
+    {
+        class Product
+        {
+            public string? Name { get; set; }
+            public double Price { get; set; }
+            public int ID { get; set; }
+            public string? Origin { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Queue<string> cachoso = new Queue<string>();
+
+            cachoso.Enqueue("Ho so 1");
+            cachoso.Enqueue("Ho so 2");
+            cachoso.Enqueue("Ho so 3");
+
+            foreach (var item in cachoso)
+            {
+                Console.WriteLine(item);
+            }
+
+            var hoso = cachoso.Dequeue();
+            Console.WriteLine($"Xu ly ho so: {hoso} - {cachoso.Count}");
+
+        }
+    }
+}
