64b836294b5e0dbe806b70956088e061e4f9143d
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-26 16:35:38 +0700

    vi du ve Stack

---
+++ bt-Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace btStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> hanghoa = new Stack<string>();
+            hanghoa.Push("mat hang 1");
+            hanghoa.Push("mat hang 2");
+            hanghoa.Push("mat hang 3");
+
+            var mathang = hanghoa.Pop();
+            Console.WriteLine($"Boc do: {mathang}  ");
+
+           mathang = hanghoa.Pop();
+            Console.WriteLine($"Boc do: {mathang}  ");
+
+            mathang = hanghoa.Pop();
+            Console.WriteLine($"Boc do: {mathang}  ");
+
+
+
+        }
+    }
+}
