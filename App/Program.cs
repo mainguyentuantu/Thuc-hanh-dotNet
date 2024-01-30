0ca45b9764bdb33b176ec30f29bd4e25336210c1
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-01-30 08:51:59 +0700

    bt MVC .Net

---
+++ App/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.WebHost.UseUrls("http://0.0.0.0:5005");
+
+// Xem xét đặt Startup.Startup  ở đây
+// đọc config
+var testoptions = builder.Configuration.GetSection("TestOptions");
+
+// Thêm vào dòng lấy IServiceCollection
+var services = builder.Services;
+/* ============================================================
+ * 
+     Copy code cũ trong Startup.ConfigureServices vào đây, ví dụ
+
+   =========================================================== */
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllersWithViews();
+}
+
+
+services.AddDistributedMemoryCache();
+services.AddSession(cfg => {
+    cfg.Cookie.Name = "xuanthulab";
+    cfg.IdleTimeout = new TimeSpan(0, 30, 0);
+});
+
+
+var app = builder.Build();
+/* ============================================================
+ * 
+    Code viết trong Configure cũ đặt tại đay, ví dụ:
+
+   =========================================================== */
+
+
+// Configure the HTTP request pipeline.
+void Confiure(IApplicationBuilder app, IWebHostEnvironment env) {
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+    else app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ btLinQ/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+using System.Threading.Tasks.Dataflow;
+
+namespace btLinQ
+{
+    public class Product
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }         // tên
+        public double Price { set; get; }        // giá
+        public string[] Colors { set; get; }     // các màu sắc
+        public int Brand { set; get; }           // ID Nhãn hiệu, hãng
+        public Product(int id, string name, double price, string[] colors, int brand)
+        {
+            ID = id; Name = name; Price = price; Colors = colors; Brand = brand;
+        }
+        // Lấy chuỗi thông tin sản phẳm gồm ID, Name, Price
+        override public string ToString()
+           => $"{ID,3} {Name,12} {Price,5} {Brand,2} {string.Join(",", Colors)}";
+
+    }
+
+    public class Brand
+    {
+        public string Name { set; get; }
+        public int ID { set; get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var brands = new List<Brand>() {
+    new Brand{ID = 1, Name = "Công ty AAA"},
+    new Brand{ID = 2, Name = "Công ty BBB"},
+    new Brand{ID = 4, Name = "Công ty CCC"},
+};
+            var products = new List<Product>()
+{
+    new Product(1, "Bàn trà",    400, new string[] {"Xám", "Xanh"},         2),
+    new Product(2, "Tranh treo", 400, new string[] {"Vàng", "Xanh"},        1),
+    new Product(3, "Đèn trùm",   500, new string[] {"Trắng"},               3),
+    new Product(4, "Bàn học",    200, new string[] {"Trắng", "Xanh"},       1),
+    new Product(5, "Túi da",     300, new string[] {"Đỏ", "Đen", "Vàng"},   2),
+    new Product(6, "Giường ngủ", 500, new string[] {"Trắng"},               2),
+    new Product(7, "Tủ áo",      600, new string[] {"Trắng"},               3),
+};
+
+            var qr = from p in products
+                     select new
+                     {
+                         name = p.Name,
+                         price = p.Price,
+                         descip = "Hoan Thanh"
+                     };
+            /* foreach (var b in qr)
+             {
+                 Console.WriteLine(b);
+             }*/
+
+            qr.ToList().ForEach(name => Console.WriteLine(name));
+
+            var qr2 = from product in products
+                      from color in product.Colors
+                      where product.Price <= 500 && color == "Xanh"
+                      orderby product.Price
+
+                      select new
+                      {
+                          Ten = product.Name,
+                          Gia = product.Price,
+                          cacmau = product.Colors
+
+                      };
+
+            qr2.ToList().ForEach(abc =>
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"{abc.Ten} - {abc.Gia} : {string.Join(',', abc.cacmau)}");
+                Console.ResetColor();
+            });
+
+            var qr3 = from p in products
+                      group p by p.Price;
+
+            qr3.ToList().ForEach(group =>
+            {
+                Console.ForegroundColor= ConsoleColor.Cyan;
+                Console.WriteLine(group.Key);
+                group.ToList().ForEach(item => Console.WriteLine(item));
+                Console.ResetColor() ;
+            }
+            );
+
+        }
+    }
+}
